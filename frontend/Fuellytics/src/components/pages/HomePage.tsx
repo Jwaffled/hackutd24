33b0f0e8a50a98b9d782296359f9df34761afdca
Huy Nong <Huy.Nong.219@gmail.com>
2024-11-17 11:06:21 -0600

    Quick commit

--- frontend/Fuellytics/src/components/pages/HomePage.tsx
+++ frontend/Fuellytics/src/components/pages/HomePage.tsx
@@ -58,9 +58,9 @@ const HomePage = () => {
                     Aggregates
                   </button>
                 </a>
-                <a href="/examples">
+                <a href="/aboutus">
                   <button className="w-full px-4 py-2 text-sm font-medium text-white bg-red-600 rounded-md hover:bg-blue-700 transition-colors">
-                    Examples
+                    About Us
                   </button>
                 </a>
                 <a href="/models">
